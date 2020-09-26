47cc5db64aedd74095daede1657e8c1ca7779760
Kamran Ahmed <kamranqdn@gmail.com>
2020-09-26 23:20:38 +0530

    Angular setup and http request

--- WebApplication3/Startup.cs
+++ WebApplication3/Startup.cs
@@ -29,6 +29,7 @@ namespace WebApplication3
         {
             services.AddControllers();
             services.AddDbContext<DataContext>(x => x.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -42,6 +43,7 @@ namespace WebApplication3
             //app.UseHttpsRedirection();
 
             app.UseRouting();
+            app.UseCors(policy => policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
 
             app.UseAuthorization();
 
