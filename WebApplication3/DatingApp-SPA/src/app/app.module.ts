bcc4977559ebeae06be3b12e4c57ce001613e4b2
Kamran Ahmed <kamranqdn@gmail.com>
2020-10-10 22:13:03 +0530

    last commit

--- WebApplication3/DatingApp-SPA/src/app/app.module.ts
+++ WebApplication3/DatingApp-SPA/src/app/app.module.ts
@@ -1,18 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http'
-
+import { FormsModule} from '@angular/forms'
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
